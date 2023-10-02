2066f29368e783a5383b55d1c57a5f3b58c0515b
Ruth Goh <ruth.goh@kcl.ac.uk>
2023-10-02 16:03:56 +0100

    Add some tests for Thing's description field

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Thing
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+class ThingTestCase(TestCase):
+    def setUp(self):
+        self.t1 = Thing(
+            name='t1',
+            description='the first thing',
+            quantity=3,
+        )
+        self.t1.full_clean()
+
+    def test_description_at_120_chars(self):
+        self.t1.description='x' * 120
+        try:
+            self.t1.full_clean()
+        except ValidationError:
+            self.fail('Test user is invalid.')
+
+    def test_description_max_120_chars(self):
+        self.t1.description='x' * 121
+        with self.assertRaises(ValidationError):
+                self.t1.full_clean()
