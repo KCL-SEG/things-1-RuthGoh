e04c42130dd41f1569d7b121c3e4f170d8734abe
Ruth Goh <ruth.goh@kcl.ac.uk>
2023-10-02 14:56:11 +0100

    Add Things app and home view Add and refine Thing model

---
+++ things/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
+
+class Thing(models.Model):
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+        blank=False,
+    )
+    description = models.TextField(
+        max_length=120,
+        unique=False,
+        blank=True,
+    )
+    quantity = models.PositiveIntegerField(
+        unique=False,
+        validators=[
+            MaxValueValidator(100, message='Must be <= 100.'),
+            MinValueValidator(0, message='Must be >= 0.')
+        ]
+    ) # includes 0
